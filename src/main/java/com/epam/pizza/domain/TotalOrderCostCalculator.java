048f1a8392e2a05c3e8811f80f9dd7196a6ae133
arctg <i.duran@yandex.ru>
2015-08-04 15:27:23 +0300

    junit test added

---
+++ src/main/java/com/epam/pizza/domain/TotalOrderCostCalculator.java
@@ -0,0 +1,35 @@
+package com.epam.pizza.domain;
+
+import java.util.Map;
+
+/**
+ * Created by dennis on 8/3/2015.
+ */
+public class TotalOrderCostCalculator {
+
+    public double calculateTotalOrderPrice(Map<Pizza, Integer> pizzas) {
+        double discount = 0.3;
+        double summ = 0;
+        int count = 0;
+        int max = 0;
+        if (pizzas.size() > 10 || pizzas.size() < 1) {
+            throw new IllegalArgumentException();
+        } else {
+            for (Map.Entry<Pizza, Integer> entry : pizzas.entrySet()) {
+                if (entry.getKey().getPrice() > max) {
+                    max = entry.getKey().getPrice();
+                }
+                count = count + entry.getValue().intValue();
+                summ += entry.getKey().getPrice() * entry.getValue().intValue();
+            }
+//            System.out.println("Total count of pizzas: " + count);
+            if (count > 4) {
+                summ = summ - discount * max;
+            }
+        }
+
+        return summ;
+    }
+
+
+}
